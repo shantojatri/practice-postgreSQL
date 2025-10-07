2c7c4f8696515dbbd62e75c5feb0e12c6408c606
shantojatri <shanto.jatri@gmail.com>
2025-10-07 15:13:55 +0600

    Update join queries

--- joins-examples.sql
+++ joins-examples.sql
@@ -7,47 +7,56 @@ CREATE TABLE users (
 );
 
 CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    user_id INT,
+    id SERIAL PRIMARY KEY, 
     product VARCHAR(100),
     amount INT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    user_id INT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 --? Inserting sample data into the users table
 INSERT INTO users (name, age) VALUES
 ('Alice', 30),
 ('Bob', 25),
-('Charlie', 35);    
+('Charlie', 35),
+('Hasan', 32),
+('Rupom', 28)
+;    
 
 --? Inserting sample data into the orders table
 INSERT INTO orders (user_id, product, amount) VALUES
 (1, 'Laptop', 1200),
 (2, 'Smartphone', 800),
 (1, 'Tablet', 400),
-(3, 'Monitor', 300);
+(3, 'Monitor', 1200),
+(4, 'Air-buds', 300),
+(null, 'Keyboard', 100)
+;
 
 --? Inner join example
 -- ? Retrieve all users along with their orders
--- ? Only users with orders will be included
+-- ? দুই  টেবিলে যেইগুলো মিল আছে, সেইগুলো সব আসবে। যেইগুলা নাল ভ্যালু বাদে সব আসবে 
 SELECT users.name, orders.product, orders.amount
 FROM users
 INNER JOIN orders ON users.id = orders.user_id;
 
 --? Left join example
 --? Retrieve all users and their orders, including users without orders
+--? বাঁ দিকের (users) সব ডাটা আসবে, ডান দিকে মিল থাকলে দেখাবে, না থাকলে NULL
 SELECT users.name, orders.product, orders.amount
 FROM users
 LEFT JOIN orders ON users.id = orders.user_id;
 
 --? Right join example
 --? Retrieve all orders and their associated users, including orders without users
+--? ডান দিকের (orders) সব ডাটা আসবে, বাম দিকের মিল থাকলে দেখাবে, না থাকলে NULL
 SELECT users.name, orders.product, orders.amount
 FROM users
 RIGHT JOIN orders ON users.id = orders.user_id;
 
 --? Full outer join example
 --? Retrieve all users and all orders, matching where possible
+--? দুই দিকের সব ডাটা আসবে, মিল থাকলে দেখাবে, না থাকলে NULL দিয়ে পূরণ হবে।
 SELECT users.name, orders.product, orders.amount
 FROM users
 FULL OUTER JOIN orders ON users.id = orders.user_id;
